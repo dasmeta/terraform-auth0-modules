10fbfa82e15cd49c4818142e2dc75091f0e976a0
aghamyan44 <juliaaghamyan44@gmail.com>
2022-11-16 14:31:24 +0400

    Fix(google): Disable client in google

--- modules/auth0-auth-google/main.tf
+++ modules/auth0-auth-google/main.tf
@@ -1,9 +1,9 @@
 resource "auth0_connection" "google_oauth2" {
-  name                 = "google-oauth2"
-  strategy             = "google-oauth2"
-  is_domain_connection = var.is_domain_connection
-  enabled_clients      = var.enabled_clients
-  realms = [
-    "google-oauth2",
-  ]
+  name     = "google-oauth2"
+  strategy = "google-oauth2"
+  # is_domain_connection = var.is_domain_connection
+  # enabled_clients      = var.enabled_clients
+  # realms = [
+  #   "google-oauth2",
+  # ]
 }
