238b3e70879c1ddee511ab8e45bfdf4ef64dce3d
aghamyan44 <juliaaghamyan44@gmail.com>
2022-11-17 10:47:37 +0400

    Feat(): Update version

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     auth0 = {
       source  = "auth0/auth0"
-      version = "~> 0.37.1" # Refer to docs for latest version
+      version = "~> 0.40.0" # Refer to docs for latest version
     }
   }
 }
