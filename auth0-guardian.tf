d2de76cf763eac8ec622349759290b5a40f48af8
aghamyan44 <juliaaghamyan44@gmail.com>
2022-12-07 15:33:06 +0400

    fix(): Fixs

--- auth0-guardian.tf
+++ auth0-guardian.tf
@@ -1,27 +1,16 @@
 locals {
   mfa_defaults = {
-    policy                            = "all-applications"
-    email                             = true
-    otp                               = true
-    recovery_code                     = true
-    user_verification                 = "required"
-    provider                          = "auth0"
-    message_types                     = ["sms"]
-    enrollment_message                = "{{code}} is your verification code for {{tenant.friendly_name}}. Please enter this code to verify your enrollment."
-    verification_message              = "{{code}} is your verification code for {{tenant.friendly_name}}."
-    aws_access_key_id                 = ""
-    aws_region                        = ""
-    aws_secret_access_key             = ""
-    sns_apns_platform_application_arn = ""
-    sns_gcm_platform_application_arn  = ""
-    app_name                          = null
-    apple_app_link                    = null
-    google_app_link                   = null
-    integration_key                   = ""
-    secret_key                        = ""
-    hostname                          = ""
+    policy           = "all-applications"
+    email            = true
+    otp              = true
+    recovery_code    = true
+    webauthn_roaming = "required"
+    phone            = []
+    push             = []
+    duo              = []
   }
 }
+
 module "auth0-guardian" {
   source   = "./modules/auth0-guardian"
   for_each = { for k, v in var.mfa : k => v }
@@ -30,9 +19,9 @@ module "auth0-guardian" {
   email            = lookup(each.value, "email", local.mfa_defaults.email)
   otp              = lookup(each.value, "otp", local.mfa_defaults.otp)
   recovery_code    = lookup(each.value, "recovery_code", local.mfa_defaults.recovery_code)
-  webauthn_roaming = lookup(each.value, "webauthn_roaming", [])
-  phone            = lookup(each.value, "phone", [])
-  push             = lookup(each.value, "push", [])
-  duo              = lookup(each.value, "duo", [])
+  webauthn_roaming = lookup(each.value, "webauthn_roaming", local.mfa_defaults.webauthn_roaming)
+  phone            = lookup(each.value, "phone", local.mfa_defaults.phone)
+  push             = lookup(each.value, "push", local.mfa_defaults.push)
+  duo              = lookup(each.value, "duo", local.mfa_defaults.duo)
 }
 
